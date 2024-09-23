613c2cfc1be6f3568db70b7e7ba41369d842e530
Angelo Guarracino <you@example.com"guarracinoangelo1@gmail.com>
2024-09-23 18:08:57 +0200

    Updated app.py and other related files

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ import os
 import streamlit as st
 import json
 import urllib.parse
+import locale
+import calendar
+
+# Imposta la lingua italiana
+locale.setlocale(locale.LC_TIME, 'it_IT')
+
 
 # Funzione per generare hash della password
 def hash_password(password):
@@ -121,86 +127,98 @@ def create_message(nome, cognome, tipo_visita, giorno_visita, ora_visita):
 giorni_settimana = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
 mesi = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre']
 
-# Funzione per creare la vista settimanale con pulsanti interattivi
-def create_weekly_view(df, start_date):
+# Funzione per creare la vista mensile
+def create_monthly_view(df, selected_date):
+    year, month = selected_date.year, selected_date.month
+    cal = calendar.Calendar(firstweekday=0)
+    days_in_month = list(cal.itermonthdays2(year, month))
+
     df['Data e Ora Visita'] = pd.to_datetime(df['Giorno Visita'] + ' ' + df['Ora Visita'], format='%Y-%m-%d %H:%M', errors='coerce')
     df = df.dropna(subset=['Data e Ora Visita'])
+    appointments_in_month = df[df['Data e Ora Visita'].dt.month == month]
+    
+    appointments_per_day = appointments_in_month.groupby(appointments_in_month['Data e Ora Visita'].dt.day).size().to_dict()
 
-    days_in_week = [start_date + timedelta(days=i) for i in range(7)]
-    appointments_per_day = df.groupby(df['Data e Ora Visita'].dt.date).size().to_dict()
+    st.write(f"### {calendar.month_name[month]} {year}")
 
-    weekly_data = []
-    for i, day in enumerate(days_in_week):
-        appointments = appointments_per_day.get(day.date(), 0)
-        day_of_week = giorni_settimana[day.weekday()]
-        day_display = f"<strong>{day_of_week}</strong><br>{day.day}<br>{appointments} app."
-        if st.button(f"Vedi pazienti {day_of_week}", key=f"day_{i}"):
-            st.session_state.selected_day = day.strftime('%Y-%m-%d')
-        weekly_data.append(day_display)
+    days_labels = ['Lunedì', 'Martedì', 'Mercoledì', 'Giovedì', 'Venerdì', 'Sabato', 'Domenica']
+    cols = st.columns(7)
+    for i, day_label in enumerate(days_labels):
+        cols[i].markdown(f"**{day_label}**")
 
-    return weekly_data
-
-# Funzione per mostrare i pazienti di un determinato giorno
-def show_patients_for_day(df, selected_date):
-    selected_patients = df[df['Giorno Visita'] == selected_date]
-    st.subheader(f"Appuntamenti per il giorno {datetime.strptime(selected_date, '%Y-%m-%d').strftime('%d/%m/%Y')}")
-
-    if selected_patients.empty:
-        st.write("Nessun appuntamento per questo giorno.")
-
-    else:
-
-        for index, row in selected_patients.iterrows():
-            col1, col2, col3, col4, col6, col7, col8 = st.columns([2, 2, 2, 2, 1, 1, 1])
-            col1.write(row['Nome'])
-            col2.write(row['Cognome'])
-            col3.write(row['Cellulare'])
-            col4.write(row['Tipo Visita'])
-            col6.write(row['Ora Visita'])
-
-            if col7.button('✏️', key=f"modifica_{index}"):
-                st.session_state['editing_row'] = index
-
-        if col8.button('❌', key=f"elimina_{index}"):
-            if index in df.index:  # Check if the index is valid
-                df = df.drop(index)
-                save_appointments(df, appointment_file)
-                st.success(f"Appuntamento per {row['Nome']} {row['Cognome']} eliminato correttamente!")
-                st.experimental_set_query_params(refresh=datetime.now().timestamp())
+    weeks = [days_in_month[i:i + 7] for i in range(0, len(days_in_month), 7)]
+    for week in weeks:
+        cols = st.columns(7)
+        for i, (day, day_of_week) in enumerate(week):
+            if day == 0:
+                cols[i].markdown("")
             else:
-                st.error("L'appuntamento non esiste più o è già stato eliminato.")
+                num_appointments = appointments_per_day.get(day, 0)
+                if num_appointments > 0:
+                    if cols[i].button(f"{day} ({num_appointments})", key=f"day_{day}"):
+                        st.session_state.selected_day = datetime(year, month, day).strftime('%Y-%m-%d')
+                else:
+                    cols[i].markdown(f"{day}")
 
-    # Gestisci la modifica di un appuntamento
+    if 'selected_day' in st.session_state and st.session_state.selected_day:
+        selected_day = st.session_state.selected_day
+        show_patients_for_day(df, selected_day)
+
+    # Rimuovi selected_day quando si cambia mese
+    if 'selected_day' in st.session_state and st.session_state.selected_day:
+        st.session_state.selected_day = None
+
+
+def show_patients_for_day(df, selected_date):
+    if selected_date:  # Verifica che selected_date non sia None
+        selected_patients = df[df['Giorno Visita'] == selected_date]
+        st.subheader(f"Appuntamenti per il giorno {datetime.strptime(selected_date, '%Y-%m-%d').strftime('%d/%m/%Y')}")
+
+        if selected_patients.empty:
+            st.write("Nessun appuntamento per questo giorno.")
+        else:
+            for index, row in selected_patients.iterrows():
+                col1, col2, col3, col4, col6, col7, col8 = st.columns([2, 2, 2, 2, 1, 1, 1])
+                col1.write(row['Nome'])
+                col2.write(row['Cognome'])
+                col3.write(row['Cellulare'])
+                col4.write(row['Tipo Visita'])
+                col6.write(row['Ora Visita'])
+
+                if col7.button('✏️', key=f"modifica_{index}"):
+                    st.session_state['editing_row'] = index
+
+            if col8.button('❌', key=f"elimina_{index}"):
+                if index in df.index:
+                    df = df.drop(index)
+                    save_appointments(df, appointment_file)
+                    st.success(f"Appuntamento per {row['Nome']} {row['Cognome']} eliminato correttamente!")
+                    st.experimental_set_query_params(refresh=datetime.now().timestamp())
+                else:
+                    st.error("L'appuntamento non esiste più o è già stato eliminato.")
+    else:
+        st.error("Nessun giorno selezionato.")
+
     if 'editing_row' in st.session_state:
-        # Verifica se l'indice è ancora valido
         if st.session_state['editing_row'] not in selected_patients.index:
-            st.error("Indice non valido, probabilmente l'appuntamento è stato eliminato.")
-            # Resetta lo stato solo in caso di errore per evitare altre operazioni
+            st.error("Indice non valido.")
             del st.session_state['editing_row']
         else:
-            # Usa il metodo .iloc per selezionare la riga basata sulla posizione
             row = selected_patients.iloc[st.session_state['editing_row']]
             st.subheader("Modifica l'appuntamento")
-            
-            # Input fields
             nome = st.text_input("Nome", value=row['Nome'])
             cognome = st.text_input("Cognome", value=row['Cognome'])
             cellulare = st.text_input("Cellulare", value=row['Cellulare'])
-            tipo_visita = st.selectbox("Tipo di visita", ["Terapia", "Visita posturale", "Check-up Completo"], 
-                                        key=f"selectbox_tipo_visita_{st.session_state['editing_row']}")
+            tipo_visita = st.selectbox("Tipo di visita", ["Terapia", "Visita posturale", "Check-up Completo"], key=f"selectbox_tipo_visita_{st.session_state['editing_row']}")
             giorno_visita = st.date_input("Giorno Visita", value=pd.to_datetime(row['Giorno Visita']))
 
-            # Gestione dell'orario in modo sicuro
             try:
                 ora_visita = st.time_input("Ora Visita", value=datetime.strptime(row['Ora Visita'], '%H:%M:%S').time())
             except ValueError:
                 ora_visita = st.time_input("Ora Visita", value=datetime.strptime(row['Ora Visita'], '%H:%M').time())
 
-            # Salvare le modifiche
             if st.button("Salva modifiche"):
-                # Verifica di nuovo che l'indice esista ancora per evitare accessi invalidi
                 if st.session_state['editing_row'] in selected_patients.index:
-                    # Aggiorna il DataFrame con i nuovi valori
                     df.at[selected_patients.index[st.session_state['editing_row']], 'Nome'] = nome
                     df.at[selected_patients.index[st.session_state['editing_row']], 'Cognome'] = cognome
                     df.at[selected_patients.index[st.session_state['editing_row']], 'Cellulare'] = cellulare
@@ -210,26 +228,19 @@ def show_patients_for_day(df, selected_date):
 
                     save_appointments(df, appointment_file)
                     st.success("Modifiche salvate correttamente!")
-
                 else:
                     st.error("Non è possibile salvare. L'appuntamento è stato rimosso.")
                     del st.session_state['editing_row']
 
-                    # Resetta lo stato solo dopo il salvataggio
-                    del st.session_state['editing_row']
-
-# Interfaccia utente con login
 st.title("Portale Appuntamenti")
 
 if 'logged_in' not in st.session_state:
     st.session_state.logged_in = False
 
-# Aggiungiamo uno stato per gestire se si deve mostrare solo il form di cambio password
 if "show_change_password" not in st.session_state:
     st.session_state.show_change_password = False
 
 if not st.session_state.logged_in and not st.session_state.show_change_password:
-    # Mostra la schermata di login
     st.subheader("Accedi al portale")
     username_input = st.text_input("Nome utente")
     password = st.text_input("Password", type="password")
@@ -243,59 +254,45 @@ if not st.session_state.logged_in and not st.session_state.show_change_password:
         else:
             st.error("Credenziali non valide. Riprova.")
 elif st.session_state.show_change_password:
-    # Mostra solo il form di cambio password
     change_password(st.session_state.username)
 else:
-    # Se l'utente è loggato, mostra l'interfaccia con il calendario e altro
     st.sidebar.title(f"Benvenuta, Dott.ssa Pezzella")
     st.sidebar.write("[Logout](#)")
-
-    # Aggiungi il pulsante per mostrare la sezione di cambio password
     if st.sidebar.button("Cambia Password"):
         st.session_state.show_change_password = True
 
-    # Mostra le tabs solo se l'utente non sta cambiando la password
     df = load_appointments(appointment_file)
     df = format_appointments(df)
 
     tab1, tab2, tab3 = st.tabs(["Calendario Appuntamenti", "Nuovo Appuntamento", "Invia Messaggi WhatsApp"])
 
     with tab1:
-        if 'current_week_start' not in st.session_state:
-            st.session_state.current_week_start = datetime.now() - timedelta(days=datetime.now().weekday())
+        if 'current_month' not in st.session_state:
+            st.session_state.current_month = datetime.now().month
+        if 'current_year' not in st.session_state:
+            st.session_state.current_year = datetime.now().year
 
         col1, col2, col3 = st.columns([1, 0.05, 1])
-
         with col1:
             if st.button("←"):
-                st.session_state.current_week_start -= timedelta(weeks=1)
-        with col2:
+                if st.session_state.current_month == 1:
+                    st.session_state.current_month = 12
+                    st.session_state.current_year -= 1
+                else:
+                    st.session_state.current_month -= 1
+                st.session_state.selected_day = None  # Resetta il giorno selezionato
+
+        with col3:
             if st.button("→"):
-                st.session_state.current_week_start += timedelta(weeks=1)
+                if st.session_state.current_month == 12:
+                    st.session_state.current_month = 1
+                    st.session_state.current_year += 1
+                else:
+                    st.session_state.current_month += 1
+                st.session_state.selected_day = None  # Resetta il giorno selezionato
 
-        df = load_appointments(appointment_file)
-        df = format_appointments(df)
-
-        days_in_week = [st.session_state.current_week_start + timedelta(days=i) for i in range(7)]
-        st.markdown(f"**{days_in_week[0].strftime(f'{giorni_settimana[days_in_week[0].weekday()]} %d {mesi[days_in_week[0].month-1]} %Y')} - "
-                    f"{days_in_week[6].strftime(f'{giorni_settimana[days_in_week[6].weekday()]} %d {mesi[days_in_week[6].month-1]} %Y')}**")
-
-        cols = st.columns(7)
-        for i, day in enumerate(days_in_week):
-            day_str = day.strftime('%Y-%m-%d')
-            appointments_count = df[df['Giorno Visita'] == day_str].shape[0]
-
-            with cols[i]:
-                st.markdown(f"**{giorni_settimana[day.weekday()]}**")
-                st.markdown(f"{day.day}")
-
-                if st.button(f"{appointments_count} app.", key=f"day_{i}"):
-                    st.session_state.selected_day_index = i
-
-        if 'selected_day_index' in st.session_state:
-            selected_day = days_in_week[st.session_state.selected_day_index]
-            selected_day_str = selected_day.strftime('%Y-%m-%d')
-            show_patients_for_day(df, selected_day_str)
+        selected_date = datetime(st.session_state.current_year, st.session_state.current_month, 1)
+        create_monthly_view(df, selected_date)
 
     with tab2:
         st.header("Aggiungi un nuovo appuntamento")
